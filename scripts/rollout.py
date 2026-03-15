d32c91fdcccd6dd4087732ca36557303630b2a45
njanwani <njanwani@gatech.edu>
2026-03-15 01:08:48 -0400

    laggy srv

--- scripts/rollout.py
+++ scripts/rollout.py
@@ -5,18 +5,20 @@ import numpy as np
 import moplayground as mop
 import minimal_mjx as mm
 from pathlib import Path
+from ral import BRUCE_TRADEOFFS
 
 
 config = mm.utils.read_config()
 env, env_params = mop.envs.create_environment(
     config,
-    # manual_speed    = [0.12, 0.0, 0.0],
-    # idealistic      = True
+    manual_speed    = [0.12, 0.0, 0.0],
+    idealistic      = True
 )
 
 camera = 'track'
-directive = np.array([0.0, 1.0])
+# directive = np.array([0.0, 1.0])
 # directive = np.array([1.0, 0.0])
+directive = np.array(BRUCE_TRADEOFFS['swing_arms'])
 
 
 frames, reward_plotter, _, _ = mop.eval.rollout_policy(
