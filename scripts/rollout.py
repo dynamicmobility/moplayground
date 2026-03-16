553a62a6a1f387864b9dd9f2b6d91044ce6051d0
njanwani <njanwani@gatech.edu>
2026-03-16 14:29:55 -0400

    some dim reduction trials

--- scripts/rollout.py
+++ scripts/rollout.py
@@ -10,12 +10,16 @@ from pathlib import Path
 config = mm.utils.read_config()
 env, env_params = mop.envs.create_environment(
     config,
-    # manual_speed    = [0.12, 0.0, 0.0],
-    # idealistic      = True
+    manual_speed    = [0.12, 0.0, 0.0],
+    idealistic      = True
 )
 
 camera = 'track'
 directive = np.array([0.0, 1.0])
+directive = np.array([0.2812006 , 0.37578177, 0.03217732, 0.23709323, 0.02666323, 0.04708387]) # Low X
+# directive = np.array([7.2073470e-05, 4.2898576e-03, 4.3646526e-01, 6.0297470e-02, 5.0404664e-02, 4.4847056e-01]) # Low Y
+# directive = np.array([0.3207971 , 0.18218784, 0.0065451 , 0.370252  , 0.08222353, 0.03799446]) # High X
+# directive = np.array([1.1459584e-01, 8.2459840e-02, 1.5060634e-02, 4.0510620e-01, 2.2142822e-05, 3.8275546e-01]) # High Y
 
 
 frames, reward_plotter, _, _ = mop.eval.rollout_policy(
