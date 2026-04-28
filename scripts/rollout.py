0d728aa402b4bcef6b3efabf2685ce1b2ff105f9
njanwani <njanwani@gatech.edu>
2026-04-28 11:46:10 -0400

    update environment

--- scripts/rollout.py
+++ scripts/rollout.py
@@ -5,24 +5,24 @@ import numpy as np
 import moplayground as mop
 import minimal_mjx as mm
 from pathlib import Path
+from ral import BRUCE_TRADEOFFS, FINAL_YAMLS
 
-
-config = mm.utils.read_config()
+config = mm.utils.read_config(FINAL_YAMLS['bruce6D+DR'])
 env, env_params = mop.envs.create_environment(
     config,
     manual_speed    = [0.12, 0.0, 0.0],
-    idealistic      = True
+    idealistic      = True,
 )
 
 camera = 'track'
 directive = np.array([0.0, 1.0])
-
+directive = np.array(BRUCE_TRADEOFFS['swing_arms'])
 
 frames, reward_plotter, _, _ = mop.eval.rollout_policy(
     env         = env,
     config      = config,
     directive   = directive,
-    T           = 10.0,
+    T           = 3.0,
     camera      = camera,
     width       = 640,
     height      = 480
