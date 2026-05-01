4ecee2e864b83dadb8235fecba8c9139e80ccbdf
Varun Madabushi <vmadabushi3@gatech.edu>
2026-05-01 13:24:19 -0400

    Fixed plotting bug in plot_pareto with nondominated, as well as in draw_frontier

--- scripts/draw_frontier.py
+++ scripts/draw_frontier.py
@@ -2,6 +2,7 @@ from minimal_mjx.utils import read_config
 from moplayground.envs.create import create_environment
 from moplayground.eval.pareto import run_experiments
 from moplayground.utils.plotting import plot_pareto
+from moplayground.utils.pareto import get_nondominated
 from matplotlib import pyplot as plt
 import jax
 
@@ -22,12 +23,14 @@ rewards_over_iters, directives = run_experiments(
     only_final      = True
 )
 print(rewards_over_iters.shape)
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+nd_idx = get_nondominated(rewards_over_iters[-1], epsilon=10)
+fig, ax = plt.subplots()
 ax = plot_pareto(
     ax          = ax,
     pareto      = rewards_over_iters[-1],
     directive   = directives[-1],
-    # objective  = config.env_params
+    objective   = config.env_config.reward.optimization.objectives,
+    nondominated= nd_idx
 )
 
 plt.show()
